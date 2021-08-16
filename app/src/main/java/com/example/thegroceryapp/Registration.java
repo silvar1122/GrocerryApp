02cb82d55476494288ce17b83d256ac7160fdbb7
silvar1122 <elizasenkoro@gmail.com>
2021-08-16 06:13:56 +0300

    Initial state of the project

---
+++ app/src/main/java/com/example/thegroceryapp/Registration.java
@@ -0,0 +1,16 @@
+package com.example.thegroceryapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import com.example.thegroceryapp.R;
+
+public class Registration extends Activity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
---
+++ app/src/main/java/com/example/thegroceryapp/ui/Login_Activity.java
@@ -0,0 +1,16 @@
+package com.example.thegroceryapp.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.thegroceryapp.R;
+
+public class Login_Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
